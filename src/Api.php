0d040c95258c1fedbd4a13e0be1dce8f52b553ef
Eric Stern <eric@ericstern.com>
2018-12-18 12:13:48 -0800

    Annotate API

--- src/Api.php
+++ src/Api.php
@@ -9,9 +9,26 @@ interface Api
     const DEFAULT_TIMEOUT_MS = 3000;
     const VERSION = '0.0.3';
 
+    /**
+     * Perform a GET request to the specified endpoint.
+     *
+     * @param string $endpoint the K8S API endpoint
+     * @return array the decoded JSON from the response
+     */
     public function get(string $endpoint): array;
 
+    /**
+     * Set the network timeout. This corresponds to CURLOPT_TIMEOUT_MS
+     *
+     * @param int $timeout The network timeout, in milliseconds
+     */
     public function setTimeoutMs(int $timeout): void;
 
+    /**
+     * Set the network connect timeout. This corresponds to
+     * CURLOPT_CONNECTTIMEOUT_MS
+     *
+     * @param int $timeout The network connect timeout, in milliseconds
+     */
     public function setConnectTimeoutMs(int $timeout): void;
 }
